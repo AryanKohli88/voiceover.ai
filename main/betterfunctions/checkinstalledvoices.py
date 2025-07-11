bec89815ddf2be573e1be52d77521036c74636eb
Aryan Kohli <aryankohli0805@gmail.com>
2025-07-11 21:25:11 +0530

    cleanup

--- main/betterfunctions/checkinstalledvoices.py
+++
@@ -1,5 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    print(f'ID: {voice.id} - Name: {voice.name} - Languages: {voice.languages}')
